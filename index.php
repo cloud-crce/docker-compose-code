6d69bed24e94442b6c5276f1559ee884f29dc9c4
cloud-crce <86629408+cloud-crce@users.noreply.github.com>
2021-06-30 00:04:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+   echo "welcome to docker compose session by  Mahendra Mehra.";
+   echo "This is a PHP-MySQL demo application - Created by Mahendra Mehra.";
+   echo "<br>";
+   echo "Be safe!! stay Indoors!!";
